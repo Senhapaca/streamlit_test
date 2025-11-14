7c2551d3d10c208e227fb3a773caf599cea01a8f
Senhapaca <isabel.moreno@posgrado.ecologia.edu.mx>
2025-11-13 20:10:07 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 st.header('Lanzar una moneda')
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
 st.write('Esta aplicación ún no es funcional. Encontrucción.')
